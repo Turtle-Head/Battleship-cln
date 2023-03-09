95523bca178db80ea2dc3a9f2bb7d2f40c1473eb
James Fehr <Turtle-Head@users.noreply.github.com>
2023-03-09 14:56:18 -0800

    Made a Battleship Clone today
    simple game to test skills with getting the access of arrays down

---
+++ Battleship/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Ex1 = New Button()
+        Fire = New Button()
+        Output1 = New RichTextBox()
+        Xv = New TextBox()
+        Yv = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Ex1
+        ' 
+        Ex1.Location = New Point(21, 12)
+        Ex1.Name = "Ex1"
+        Ex1.Size = New Size(100, 23)
+        Ex1.TabIndex = 0
+        Ex1.Text = "Initialize"
+        Ex1.UseVisualStyleBackColor = True
+        ' 
+        ' Fire
+        ' 
+        Fire.Location = New Point(21, 163)
+        Fire.Name = "Fire"
+        Fire.Size = New Size(100, 23)
+        Fire.TabIndex = 1
+        Fire.Text = "Fire"
+        Fire.UseVisualStyleBackColor = True
+        ' 
+        ' Output1
+        ' 
+        Output1.Location = New Point(21, 51)
+        Output1.Name = "Output1"
+        Output1.Size = New Size(100, 96)
+        Output1.TabIndex = 2
+        Output1.Text = ""' 
+        ' Xv
+        ' 
+        Xv.Location = New Point(137, 72)
+        Xv.Name = "Xv"
+        Xv.PlaceholderText = "X val"
+        Xv.Size = New Size(46, 23)
+        Xv.TabIndex = 3
+        ' 
+        ' Yv
+        ' 
+        Yv.Location = New Point(137, 101)
+        Yv.Name = "Yv"
+        Yv.PlaceholderText = "Y val"
+        Yv.Size = New Size(46, 23)
+        Yv.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(206, 217)
+        Controls.Add(Yv)
+        Controls.Add(Xv)
+        Controls.Add(Output1)
+        Controls.Add(Fire)
+        Controls.Add(Ex1)
+        Name = "Form1"
+        Text = "Battleship"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Ex1 As Button
+    Friend WithEvents Fire As Button
+    Friend WithEvents Output1 As RichTextBox
+    Friend WithEvents Xv As TextBox
+    Friend WithEvents Yv As TextBox
+End Class
---
+++ Battleship/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    'Author: James Fehr 2023-03-09
+    'Battleship Project
+    '-----------------------------------------------------------------------------------
+    Dim GBA(,) As String ' = {{"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}} 'array of Os
+    Sub PrintBoard()
+        Output1.Clear()
+        For Index = 0 To GBA.GetUpperBound(0)                       'starting for loop
+            For innerIndex = 0 To GBA.GetUpperBound(1)           'run loop from zero => length of array
+                Output1.Text += GBA(Index, innerIndex)              'output array value
+                ' Output1.Text += vbCrLf
+            Next
+            Output1.Text += vbCrLf                                  'add a line return to output 
+        Next
+
+    End Sub
+
+    Private Sub Ex1_Click(sender As Object, e As EventArgs) Handles Ex1.Click
+        'Initialize Gameboard
+
+
+        GBA = New String(4, 4) {{"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}} 'array of Os
+        PrintBoard()
+        MessageBox.Show("Battleship Initialized")                              'Message the output value for user
+
+    End Sub
+    Private Sub Fire_Click(sender As Object, e As EventArgs) Handles Fire.Click
+        Dim Xval As Integer
+        Xval = (Convert.ToInt32(Xv.Text))       'gets and converts values to integers
+        Dim Yval As Integer
+        Yval = (Convert.ToInt32(Yv.Text))       'gets and converts values to integers
+        If (Xval = 0 And Yval = 0) Then  'checks to see if x,y coordinates are in bounds
+            GBA(Yval, Xval) = "Ⓧ"       'if in bounds changes item in array to hit
+        ElseIf (Xval = 0 Or Xval <= 4) And (Yval = 0 Or Yval <= 4) Then
+            GBA(Yval, Xval) = "Ⓧ"       'if in bounds changes item in array to hit
+        ElseIf (Xval > 4) Then     'checks if out of bounds
+            MessageBox.Show("Out of bounds")
+            Xv.Text = ""                        'resets Xv coordinate
+            'Yv.Text = ""                        'resets Yv coodinate
+        ElseIf (Yval > 4) Then     'checks if out of bounds
+            MessageBox.Show("Out of bounds")
+            'Xv.Text = ""                        'resets Xv coordinate
+            Yv.Text = ""                        'resets Yv coodinate
+        End If
+        PrintBoard()
+    End Sub
+
+
+End Class
