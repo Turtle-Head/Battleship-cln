93b4a41c2dc6cdd289f9ffa1fe6ab9c95a7aa501
James Fehr <Turtle-Head@users.noreply.github.com>
2023-03-09 21:28:58 -0800

    Updating Style
    -Adjusted the size of the gameboard
    -Added color and background
    -Changed Hit representative
    -Changed Initial array values

--- Battleship/Form1.Designer.vb
+++ Battleship/Form1.Designer.vb
@@ -31,21 +31,25 @@ Partial Class Form1
         ' 
         ' Ex1
         ' 
-        Ex1.Location = New Point(21, 12)
+        Ex1.BackColor = Color.MidnightBlue
+        Ex1.ForeColor = Color.Gold
+        Ex1.Location = New Point(33, 12)
         Ex1.Name = "Ex1"
-        Ex1.Size = New Size(100, 23)
+        Ex1.Size = New Size(125, 23)
         Ex1.TabIndex = 0
         Ex1.Text = "Initialize"
-        Ex1.UseVisualStyleBackColor = True
+        Ex1.UseVisualStyleBackColor = False
         ' 
         ' Fire
         ' 
-        Fire.Location = New Point(21, 163)
+        Fire.BackColor = Color.DarkBlue
+        Fire.ForeColor = Color.Gold
+        Fire.Location = New Point(33, 163)
         Fire.Name = "Fire"
-        Fire.Size = New Size(100, 23)
+        Fire.Size = New Size(124, 23)
         Fire.TabIndex = 3
         Fire.Text = "Fire"
-        Fire.UseVisualStyleBackColor = True
+        Fire.UseVisualStyleBackColor = False
         ' 
         ' Output1
         ' 
@@ -53,28 +57,32 @@ Partial Class Form1
         Output1.Cursor = Cursors.Hand
         Output1.Font = New Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
         Output1.ForeColor = Color.Gold
-        Output1.Location = New Point(21, 51)
+        Output1.Location = New Point(51, 51)
         Output1.Name = "Output1"
         Output1.ReadOnly = True
         Output1.ScrollBars = RichTextBoxScrollBars.None
-        Output1.Size = New Size(100, 96)
+        Output1.Size = New Size(68, 96)
         Output1.TabIndex = 2
         Output1.TabStop = False
         Output1.Text = ""' 
         ' Xv
         ' 
-        Xv.Location = New Point(137, 72)
+        Xv.BackColor = Color.Navy
+        Xv.ForeColor = Color.Gold
+        Xv.Location = New Point(132, 52)
         Xv.Name = "Xv"
-        Xv.PlaceholderText = "X val"
-        Xv.Size = New Size(46, 23)
+        Xv.PlaceholderText = "X"
+        Xv.Size = New Size(14, 23)
         Xv.TabIndex = 1
         ' 
         ' Yv
         ' 
-        Yv.Location = New Point(137, 101)
+        Yv.BackColor = Color.Navy
+        Yv.ForeColor = Color.Gold
+        Yv.Location = New Point(132, 124)
         Yv.Name = "Yv"
-        Yv.PlaceholderText = "Y val"
-        Yv.Size = New Size(46, 23)
+        Yv.PlaceholderText = "Y"
+        Yv.Size = New Size(14, 23)
         Yv.TabIndex = 2
         ' 
         ' Form1
@@ -82,13 +90,15 @@ Partial Class Form1
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.MidnightBlue
-        ClientSize = New Size(206, 217)
+        BackgroundImage = My.Resources.Resources.abstract_7430672_640
+        ClientSize = New Size(192, 217)
         Controls.Add(Yv)
         Controls.Add(Xv)
         Controls.Add(Output1)
         Controls.Add(Fire)
         Controls.Add(Ex1)
         Name = "Form1"
+        ShowIcon = False
         Text = "Battleship"
         ResumeLayout(False)
         PerformLayout()
--- Battleship/Form1.vb
+++ Battleship/Form1.vb
@@ -2,7 +2,7 @@
     'Author: James Fehr 2023-03-09
     'Battleship Project
     '-----------------------------------------------------------------------------------
-    Dim GBA(,) As String ' = {{"*", "*", "*", "*", "*"}, {"*", "*", "*", "*", "*"}, {"*", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}} 'array of Os
+    Dim GBA(,) As String ' = {{"*", "*", "*", "*", "*"}, {"◆", "◆", "◆", "◆", "◆"}, {"⌂", "⌂", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}} 'array of Os
     Sub PrintBoard()
         Output1.Clear()
         For Index = 0 To GBA.GetUpperBound(0)                       'starting for loop
@@ -19,7 +19,7 @@
         'Initialize Gameboard
 
 
-        GBA = New String(4, 4) {{"◎", "◎", "◎", "◎", "◎"}, {"◎", "◎", "◎", "◎", "◎"}, {"◎", "◎", "◎", "◎", "◎"}, {"◎", "◎", "◎", "◎", "◎"}, {"◎", "◎", "◎", "◎", "◎"}} 'array of Os
+        GBA = New String(4, 4) {{"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}, {"ⓞ", "ⓞ", "ⓞ", "ⓞ", "ⓞ"}} 'array of Os
         PrintBoard()
         MessageBox.Show("Battleship Initialized")                              'Message the output value for user
 
@@ -29,10 +29,10 @@
         Xval = (Convert.ToInt32(Xv.Text))       'gets and converts values to integers
         Dim Yval As Integer
         Yval = (Convert.ToInt32(Yv.Text))       'gets and converts values to integers
-        If (Xval = 0 And Yval = 0) Then  'checks to see if x,y coordinates are in bounds
-            GBA(Yval, Xval) = "Ⓧ"       'if in bounds changes item in array to hit
-        ElseIf (Xval = 0 Or Xval <= 4) And (Yval = 0 Or Yval <= 4) Then
-            GBA(Yval, Xval) = "Ⓧ"       'if in bounds changes item in array to hit
+        If (Xval = 0 And Yval = 0) Then  'evaluates if x,y coordinates are in bounds
+            GBA(Yval, Xval) = "◆"       'if in bounds changes item in array to hit
+        ElseIf (Xval = 0 Or Xval <= 4) And (Yval = 0 Or Yval <= 4) Then 'evaluates if x,y are in bounds
+            GBA(Yval, Xval) = "◆"       'if in bounds changes item in array to hit
         ElseIf (Xval > 4) Then     'checks if out of bounds
             MessageBox.Show("Out of bounds")
             Xv.Text = ""                        'resets Xv coordinate
